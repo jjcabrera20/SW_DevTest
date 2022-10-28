01c8077eeff408d11ebb4841ff2e09b5e6effe53
jjcabrera20 <jjcabrera20@yahoo.com.mx>
2022-10-28 11:53:07 -0500

    third

--- MIDAS_TEST/beneficiaries_app/url.py
+++ MIDAS_TEST/beneficiaries_app/url.py
@@ -7,6 +7,7 @@ app_name ='beneficiaries_app'
 urlpatterns = [
     path('', views.index, name='wellcome'),
     path('beneficiary_new/', views.beneficiary_new, name='beneficiary_new'),
-    #path('persona_edit/<int:persona_id>/', views.persona_edit, name='persona_edit'),
+    path('beneficiary_edit/<int:beneficiaries_id>/', views.beneficiary_edit, name='beneficiary_edit'),
+    path('beneficiary_delete/<int:beneficiaries_id>/', views.beneficiary_delete, name='beneficiary_delete'),
     path('beneficiary_list/', views.beneficiary_list, name='beneficiary_list'),
 ]
--- MIDAS_TEST/beneficiaries_app/views.py
+++ MIDAS_TEST/beneficiaries_app/views.py
@@ -22,6 +22,33 @@ def beneficiary_new(request):
     return render(request, 'beneficiary_edit.html', {'form': form})
 
 
+def beneficiary_edit(request, beneficiaries_id):
+    beneficiary = get_object_or_404(Beneficiaries, id=beneficiaries_id)
+    if request.method == 'POST':
+        form = BeneficiariesForm(request.POST, instance=beneficiary)
+        if form.is_valid():
+            p1 = form.save()
+            return redirect('beneficiaries_app:beneficiary_list')
+        else:
+            messages.error(request, form.errors)
+    else:
+        form = BeneficiariesForm(instance=beneficiary)
+    context = {'form': form}
+    return render(request, 'beneficiary_edit.html', context)
+
+
+def beneficiary_delete(request, beneficiaries_id):
+    beneficiary = get_object_or_404(Beneficiaries, id=beneficiaries_id)
+    context = {
+        "beneficiary": beneficiary
+    }
+    if request.method == 'POST':
+        beneficiary.delete()
+        return redirect('beneficiaries_app:beneficiary_list')
+    else:
+        return render(request, "beneficiary_delete.html", context)
+
+
 def beneficiary_list(request):
     beneficiaries = Beneficiaries.objects.all()
     context = {'beneficiaries': beneficiaries}
